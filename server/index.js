568dcc4166bd841bcb8252b3b9d4847aa356ba88
Serhii B <s_blyshchak@ukr.net>
2023-11-17 20:01:09 +0200

    implement register logic

--- server/index.js
+++ server/index.js
@@ -2,15 +2,14 @@ const express = require("express")
 const cors = require("cors")
 const mongoose = require("mongoose")
 
+const userRoute = require("./Routes/userRoute")
+
 const app = express()
 require("dotenv").config()
 
 app.use(express.json())
 app.use(cors())
-
-app.get("/", (req, res) => {
-  res.send("yo bro")
-})
+app.use("/api/users", userRoute)
 
 const port = process.env.PORT || 5000
 const uri = process.env.ATLAS_URI
