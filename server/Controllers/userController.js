68e3dc992101b4de8b7eac64eaf97674139a3b74
Serhii B <s_blyshchak@ukr.net>
2023-11-17 20:47:37 +0200

    implement login, get user and get users

--- server/Controllers/userController.js
+++ server/Controllers/userController.js
@@ -7,7 +7,7 @@ const createToken = (_id) => {
   const jwtkey = process.env.JWT_SECRET_KEY
 
   return jwt.sign({ _id }, jwtkey, { expiresIn: "3d" })
-}    
+}
 
 const registerUser = async (req, res) => {
   try {
@@ -36,4 +36,52 @@ const registerUser = async (req, res) => {
   }
 }
 
-module.exports = { registerUser }
+const loginUser = async (req, res) => {
+  const { email, password } = req.body
+
+  let user = await userModel.findOne({ email })
+
+  if (!user) return res.status(400).json("Invalid email or password")
+
+  const isValidPassword = await bcrypt.compare(password, user.password)
+
+  if (!isValidPassword) return res.status(400).json("Invalid email or password")
+
+  const token = createToken(user._id)
+
+  res.status(200).json({ _id: user._id, name: user.name, email, token })
+
+  try {
+  } catch (error) {
+    console.log(error)
+    res.status(500).json(error)
+  }
+}
+
+const findUser = async (req, res) => {
+  const userId = req.params.userId
+
+  try {
+    const user = await userModel.findById(userId)
+
+    if (!user) return res.status(400).json("User not find")
+
+    res.status(200).json(user)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json(error)
+  }
+}
+
+const getUsers = async (req, res) => {
+  try {
+    const users = await userModel.find()
+
+    res.status(200).json(users)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json(error)
+  }
+}
+
+module.exports = { registerUser, loginUser, findUser, getUsers }
